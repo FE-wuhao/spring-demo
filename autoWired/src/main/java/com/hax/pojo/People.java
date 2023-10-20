4b9f946cf3c3ef1f2181dff4b98f04486b95066f
吴灏 <wuh@cnlemon.net>
2023-10-20 17:55:30 +0800

    feat: 通过AutoWire注解实现自动装配

--- autoWired/src/main/java/com/hax/pojo/People.java
+++ autoWired/src/main/java/com/hax/pojo/People.java
@@ -1,8 +1,14 @@
 package com.hax.pojo;
 
-public class People {
-    private Dog dog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
+public class People {
+    @Autowired
+    private Dog dog;
+    @Autowired
+    // 当自动装配byType匹配不到时，手动指定id，即可实现自动装配
+    @Qualifier("cat111")
     private Cat cat;
 
     public Dog getDog() {
